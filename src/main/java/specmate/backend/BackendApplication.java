61532139c421ea70c1ad3a5a6916e5c4766bc6cf
정인호 <jeonginho@jeong-inhoui-MacBookAir.local>
2025-09-16 11:52:21 +0900

    feat: db service remove

--- src/main/java/specmate/backend/BackendApplication.java
+++ src/main/java/specmate/backend/BackendApplication.java
@@ -1,23 +1,12 @@
 package specmate.backend;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import specmate.backend.service.product.ProductImportService;
 
 @SpringBootApplication
-@RequiredArgsConstructor
-public class BackendApplication implements CommandLineRunner {
-
-    private final ProductImportService importService;
+public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-        importService.importFromJsonOnce();
     }
 }
